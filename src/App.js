59485dc8fecd891254bab7bdce57ed7a4e11b6ca
nakuldevkhajuria <nakuldevkhajuria@gmail.com>
2023-01-24 19:20:01 +0530

    second Handson

--- src/App.js
+++ src/App.js
@@ -1,23 +1,96 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  // const [toggle,setToggle] = useState(false);
+  const [formData,setFormData] = useState(
+    {
+      Name:'',
+      Department : '',
+      Rating : '',
+      myEmployees : []
+    }
+  )
+  function submitChange(e){
+
+//     let empObj = {
+//       Name: formData.Name,
+//       Department : formData.Department,
+//       Rating : formData.Rating
+//     }
+//     const arr = formData.myEmployees;
+//     arr.push(empObj);
+//     setFormData({myEmployees: arr})//setting the arr value to the myEmpoyees
+// // setFormData({Name:'',Department:'',Rating:''}
+
+//     console.log(formData)
+
+e.preventDefault();
+    setFormData((prevFormData) => {
+        const empObj = {
+            Name: prevFormData.Name,
+            Department: prevFormData.Department,
+            Rating: prevFormData.Rating,
+        };
+        return {
+            myEmployees: [...prevFormData.myEmployees, empObj],
+            Name: '',
+            Department: '',
+            Rating: '',
+        };
+    });
+  }
+  
+  function changeValue(e){
+   
+     e.preventDefault();
+  
+          // const newFormData = { ...formData,[e.target.name] : e.target.value };
+          const newFormData = {...formData};
+      
+          newFormData[e.target.name] = e.target.value;
+          setFormData(newFormData);
+
+        
+
+    // this.setState({[e.target.name] : e.target.value})
+// console.log(e)
+// setFormData( (e) => ( {[e.target.name]:e.target.value}))
+console.log(formData)
+  }
+   
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 >EMPLOYEE FEEDBACK FORM</h1>
+      <form className='form'>
+      <label>Name : </label>
+      <input type='text' name='Name' value={formData.Name} onChange={changeValue}></input> <br/>
+      <label>Department : </label>
+      <input type='text' name='Department' value={formData.Department} onChange={changeValue}></input> <br/>
+      <label>Rating : </label>
+      <input type='text' name='Rating' value={formData.Rating} onChange={changeValue}></input> <br/>
+      <button type='button' onClick={submitChange}>Submit</button>
+      {/* <input type='submit' value='Submit' /> */}
+      </form>
+      <div className='list'>
+{formData.myEmployees.map((value,index)=> {
+
+  return (
+
+      <div key={index} className='wholeDiv' >
+    
+     
+       Name : {value.Name} | Department : {value.Department} | Rating : {value.Rating}
+
+
+
+
+    </div> 
+    
+  )
+})}
+      </div>
     </div>
   );
 }
---
+++ src/Components/AddStudent.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function AddStudent() {
+  return (
+    <div>
+<label htmlFor="">Name : </label>
+
+    </div>
+  )
+}
